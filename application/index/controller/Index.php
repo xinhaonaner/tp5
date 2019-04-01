678bc0388a0faafa6518a532988112afd0d81728
fzp <Fzhengpei@gmail.com>
2019-04-01 18:48:23 +0800

    reset

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\index\controller;
 
 class Index
@@ -9,8 +10,13 @@ class Index
             ['name' => '小王', 'age' => 12],
             ['name' => '小张', 'age' => 13],
         ];
+        $data = [
+            'code' => 200,
+            'data' => $list,
+            'msg' => 'success',
+        ];
 
-        return json($list);
+        return json($data);
         // exit();
         // return $this->result($list, 200, 'success');
         // return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:) </h1><p> ThinkPHP V5.1<br/><span style="font-size:30px">12载初心不改（2006-2018） - 你值得信赖的PHP框架</span></p></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=64890268" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="eab4b9f840753f8e7"></think>';
